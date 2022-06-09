5a795fee1b8238982f77512fd68b2a883a54ae0d
vk7050 <84660046+vk7050@users.noreply.github.com>
2022-06-09 13:50:58 +0530

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -1,8 +1,22 @@
 //print nodes at distance k in binary tree
 #include<bits/stdc++.h>
 using namespace std;
+struct Node
+{
+  int data;
+  Node *left,right;
+  Node(int x)
+  {
+    data=x;
+    left=right=NULL;
+  }
+};
 int main()
 {
-  cout<<"welcome in github"<<endl;
+  Node *root=Node(1);
+  root->left=Node(2);
+  root->right=Node(3);
+  root->left->left=Node(4);
+  root->left->right=Node(5);
   return 0;
 }

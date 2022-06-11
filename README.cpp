f472a7701933de0baa1431dddcefdc5040d24985
vk7050 <84660046+vk7050@users.noreply.github.com>
2022-06-11 15:35:35 +0530

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -4,7 +4,7 @@ using namespace std;
 struct Node
 {
   int data; 
-  Node *left,right;
+  Node *left,*right;
   Node(int x)
   {
     data=x;
@@ -25,11 +25,11 @@ void print(Node *root,int k)
 
 int main()
 {
-  Node *root=Node(1);
-  root->left=Node(2);
-  root->right=Node(3);
-  root->left->left=Node(4);
-  root->left->right=Node(5);
+  Node *root=new Node(1);
+  root->left=new Node(2);
+  root->right=new Node(3);
+  root->left->left=new Node(4);
+  root->left->right=new Node(5);
   print(root,2);
   return 0;
 }
